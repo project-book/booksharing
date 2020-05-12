2e2d203db46d15f5e086b99fe988f5e70cb522b0
giofrac <fraccavento.84@hotmail.it>
2020-05-12 12:21:44 +0200

    ok

--- Foundation/utility/config.php
+++ Foundation/utility/config.php
@@ -1,6 +1,12 @@
 <?php
+$config['mysql']['user'] = 'root';
+$config['mysql']['password'] = '';
+$config['mysql']['host'] = 'localhost';
+$config['mysql']['database'] = 'booksharing';
+
 $hostname = "127.0.0.1";
-$dbname = "book_sharing";
-$user ="root";
+$dbname = "booksharing";
+$user = "root";
 $pass = "";
+
 ?>
