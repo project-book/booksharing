5255de00b985a2db788586f5493637a71927a7fa
fedepaolo <federicopaolonefederico@gmail.com>
2020-05-13 16:33:14 +0200

    ok

---
+++ StartSmarty.php
@@ -0,0 +1,15 @@
+<?php
+require_once'smarty/Smarty.class.php';
+class StartSmarty
+{
+    static function configuration()
+    {
+        $smarty=new smarty();
+        $smarty->template_dir='smarty/templates/';
+        $smarty->compile_dir='smarty/templates_c/';
+        $smarty->config_dir='smarty/configs/';
+        $smarty->cache_dir='smarty/cache/';
+        return $smarty;
+    }
+}
+?>
